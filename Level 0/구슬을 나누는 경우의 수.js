1f506b2081cf6158042ea35e48984d67a5b437ff
geunseonkim <guensoen@gmail.com>
2024-07-18 17:12:51 +1200

    입문문제 day9

--- Level 0/구슬을 나누는 경우의 수.js
+++ Level 0/구슬을 나누는 경우의 수.js
@@ -1,26 +1,47 @@
 // function solution(balls, share) {
-//     function factorial(n) {
-//         let result = 1;
-//         for (let i = 1; i <= n; i ++) {
-//             result *= i
-//         }
-//         return result
+//   function factorial(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//       result *= i;
 //     }
-//     return Math.round(factorial(balls) / (factorial(balls-share) * factorial(share)))
+//     return result;
+//   }
+//   return Math.round(
+//     factorial(balls) / (factorial(balls - share) * factorial(share))
+//   );
+// }
+
+// function solution(balls, share) {
+//   if (balls === share) {
+//     return 1;
+//   }
+
+//   let 분자 = 1;
+//   let 분모 = 1;
+
+//   for (let i = 0; i < share; i++) {
+//     분자 *= balls - i;
+//     분모 *= i + 1;
+//   }
+
+//   return Math.round(분자 / 분모);
 // }
 
 function solution(balls, share) {
-  if (balls === share) {
-    return 1;
+  let result = 1;
+  while (share > 0) {
+    result *= balls / share;
+    balls -= 1;
+    share -= 1;
   }
-
-  let numerator = 1;
-  let denominator = 1;
-
-  for (let i = 0; i < share; i++) {
-    numerator *= balls - i;
-    denominator *= i + 1;
-  }
-
-  return Math.round(numerator / denominator);
+  return Math.round(result);
 }
+
+// const factorial = (n) => {
+//   n === 0 ? 1 : n * factorial(n); // factorial 재귀함수
+// };
+// function solution(balls, share) {
+//   return Math.round(
+//     factorial(balls) / factorial(balls - share) / factorial(share)
+//   );
+// }
