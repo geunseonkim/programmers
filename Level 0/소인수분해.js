9c944bb7026c66c0847580687e202b84676074f3
geunseonkim <guensoen@gmail.com>
2024-07-22 00:18:22 +1200

    입문문제 day12

--- Level 0/소인수분해.js
+++ Level 0/소인수분해.js
@@ -1,5 +1,6 @@
-// // 소인수분해: 어떤 수를 소수들의 곱으로 표현하는 것. (ex: 12를 소인수분해하면 소인수는 2, 3)
-// // 주어진 수의 소인수를 구하는 함수 === 주어진 수의 약수이면서 소수인 경우를 배열로 반환하는 함수.
+// 약수면서 소수인 것만 찾기.
+// 주어진 수의 약수이면서 소수인 경우를 배열로 반환하는 함수.
+
 // function solution(n) {
 //   // 소수 검사.
 //   function isPrime(num) {
@@ -20,7 +21,39 @@
 //   return result;
 // }
 
-// // 주어진 수까지의 소수를 배열로 반환하는 함수.
+// 소인수분해로 모든 소인수 찾기. -> 소인수분해: 어떤 수를 소수들의 곱으로 표현하는 것. (ex: 12를 소인수분해하면 소인수는 2, 3)
+
+// (1)
+// function solution(n) {
+//   let answer = [];
+
+//   let i = 2;
+//   while (i <= n) {
+//     if (n % i === 0) {
+//       answer.push(i);
+//       n = n / i; // 소인수 i를 여러 번 나누었고, i가 더 이상 n의 소인수가 아니면 다음 소인수로 넘어가게끔 함. so 소인수들이 중복돼도 정확한 소인수 리스트를 생성할 수 있도록 해준다.
+//     } else {
+//       i++;
+//     }
+//   }
+//   return [...new Set(answer)];
+// }
+
+// (2)
+function solution(n) {
+  let answer = [];
+
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      answer.push(i);
+      n = n / i;
+    }
+  }
+  return [...new Set(answer)];
+}
+
+// cf.
+// 주어진 수까지의 소수를 배열로 반환하는 함수.
 // function findPrimes(n) {
 //   const isPrime = (num) => {
 //     if (num < 2) return false;
@@ -40,7 +73,7 @@
 // }
 // console.log(findPrimes(30)); // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-// // 주어진 수의 약수를 배열로 반환하는 함수.
+// 주어진 수의 약수를 배열로 반환하는 함수.
 // function findDivisors(n) {
 //   let divisors = [];
 //   for (let i = 1; i <= n; i++) {
