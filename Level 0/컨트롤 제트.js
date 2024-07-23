8723c835177fa92322e9d87c0d8485cd631d139d
geunseonkim <guensoen@gmail.com>
2024-07-23 23:50:16 +1000

    입문문제 day13

--- Level 0/컨트롤 제트.js
+++ Level 0/컨트롤 제트.js
@@ -3,3 +3,20 @@ function solution(s) {
   const result = arr.filter((v, i) => v !== "Z" && arr[i + 1] !== "Z");
   return result.map(Number).reduce((acc, cur) => acc + cur, 0);
 }
+
+// 다른 방식.
+function solution(s) {
+  const stack = [];
+  s.split(" ").forEach((target) => {
+    if (target === "Z") stack.pop();
+    else stack.push(+target);
+  });
+  return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
+function solution(s) {
+  let arr = s.split(" ");
+  while (arr.indexOf("Z") > -1) {
+    arr.splice(arr.indexOf("Z") - 1, 2);
+  }
+  return arr.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+}
