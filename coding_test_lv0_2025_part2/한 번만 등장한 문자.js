a3234d7f689a5a7a5d64a36512160da38ef419a7
geunseonkim <guensoen@gmail.com>
2025-03-20 00:19:50 +1300

    checked if char appears once using indexOf and lastIndexOf

---
+++ coding_test_lv0_2025_part2/한 번만 등장한 문자.js
@@ -0,0 +1,25 @@
+// [programmers - 한 번만 등장한 문자](https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=javascript)
+
+// Solution
+function solution(s) {
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      answer += s[i];
+    }
+  }
+  return answer.split("").sort().join("");
+}
+
+/* Review
+  
+s.indexOf(s[i]) === s.lastIndexOf(s[i])
+
+Used indexOf and lastIndexOf to check if a character appears only once in the string
+*/
+
+function solution(s) {
+  let answer = [];
+  for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) answer.push(c);
+  return answer.sort().join("");
+}
