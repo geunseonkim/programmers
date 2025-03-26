f0545ac79d184e13225eaedcb574950a82639b62
geunseonkim <guensoen@gmail.com>
2025-03-27 10:00:03 +1300

    calculated message length for letter width

---
+++ coding_test_lv0_2025_part2/편지.js
@@ -0,0 +1,15 @@
+// [programmers - 편지](https://school.programmers.co.kr/learn/courses/30/lessons/120898?language=javascript)
+
+// Solution
+function solution(message) {
+  var answer = 0;
+  answer = message.length * 2;
+  return answer;
+}
+
+/* Review
+    Simple and straightforward. nothing special.
+    */
+function solution(message) {
+  return message.length * 2;
+}
