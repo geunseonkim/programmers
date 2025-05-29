2ae6b6529ce8e0c97a99bd155c7c21e565f19a49
geunseonkim <guensoen@gmail.com>
2025-05-29 22:40:34 +0900

    solve with m*n -1 as minimum cuts

---
+++ coding_test_lv0_2025_part2/종이 자르기.js
@@ -0,0 +1,12 @@
+// [programmers - 종이 자르기](https://school.programmers.co.kr/learn/courses/30/lessons/120922?language=javascript)
+
+// Solution
+function solution(M, N) {
+  // if (M===1 && N===1) return 0
+  // return (M - 1) + ((N - 1) * M)
+  return M * N - 1;
+}
+
+/* Review
+    Simple and straightforward.
+ */
