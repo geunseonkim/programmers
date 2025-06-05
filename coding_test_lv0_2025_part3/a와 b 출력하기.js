c0f1134da77894cf00d7d2fb8ce7f4437d848e8c
geunseonkim <guensoen@gmail.com>
2025-06-05 09:00:35 +0900

    refactor: use destructuring

--- coding_test_lv0_2025_part3/a와 b 출력하기.js
+++ coding_test_lv0_2025_part3/a와 b 출력하기.js
@@ -22,14 +22,14 @@ const [a, b] = line.split[""];
  */
 
 // simply way
-// const readline = require("readline");
-// const rl = readline
-//   .createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   })
-//   .on("line", function () {
-//     const [a, b] = line.split[""];
-//     console.log("a =", a);
-//     console.log("b =", b);
-//   });
+const readline = require("readline");
+const rl = readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", function (line) {
+    const [a, b] = line.split(" ");
+    console.log("a =", a);
+    console.log("b =", b);
+  });
