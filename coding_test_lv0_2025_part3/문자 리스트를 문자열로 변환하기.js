18bbc051e5d2f47c5a0443198ada9b0568c5dfef
geunseonkim <guensoen@gmail.com>
2025-06-26 20:53:58 +0900

    solved with join()

---
+++ coding_test_lv0_2025_part3/문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,9 @@
+// [programmers - 문자 리스트를 문자열로 변환하기](https://school.programmers.co.kr/learn/courses/30/lessons/181941)
+
+// Solution
+function solution(arr) {
+  return arr.join("");
+}
+/* Review
+Simple and straightforward. nothing special.
+ */
