66923b1fc14766dae05674dd04a24dc582eaa0eb
geunseonkim <guensoen@gmail.com>
2025-07-10 09:26:52 +0900

    feat: implement conditional sum/difference

---
+++ coding_test_lv0_2025_part3/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,10 @@
+// [programmers - flag에 따라 다른 값 반환하기](https://school.programmers.co.kr/learn/courses/30/lessons/181933)
+
+// Solution
+function solution(a, b, flag) {
+  return flag ? a + b : a - b;
+}
+
+/* Review
+Simple and straightforward. nothing special.
+ */
